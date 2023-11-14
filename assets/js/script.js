b078e3f9424ea74b4049b3a14ef53f036ed2457f
SimonMertins <>
2023-11-14 10:45:18 +0000

    correct number show,need to makke it update after every guess

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,33 +14,39 @@ document.addEventListener('DOMContentLoaded', function () {
     let correctGuesses = 0;
     let incorrectGuesses = 0;
   
-    // Click event listener for submit button
     submitButton.addEventListener('click', function () {
-      // User guess
-      const userGuess = parseInt(answerBox.value);
-  
-      // Check if the guess is correct
-      if (!isNaN(userGuess) && userGuess >= 1 && userGuess <= 6) {
-        if (userGuess === correctNumber) {
-          
-          correctGuesses++;
+        // User guess
+        const userGuess = parseInt(answerBox.value);
+    
+        // Check if the guess is correct
+        if (!isNaN(userGuess) && userGuess >= 1 && userGuess <= 6) {
+            if (userGuess === correctNumber) {
+                // Correct guess
+                correctGuesses++;
+    
+                // Show the correct number
+                const correctNumberElement = document.getElementById('correct-number');
+                correctNumberElement.textContent = correctNumber;
+    
+                // Show the correct number message
+                const correctNumberMessage = document.querySelector('.correct-number-message');
+                correctNumberMessage.style.display = 'block';
+            } else {
+                // Incorrect guess
+                incorrectGuesses++;
+            }
+    
+            // Update the scores
+            scoreElement.textContent = correctGuesses;
+            incorrectElement.textContent = incorrectGuesses;
+    
+            // Reset the answer box for the next guess
+            answerBox.value = '';
+    
+            // Generate a new random number for the next round
+            correctNumber = Math.floor(Math.random() * 6) + 1;
         } else {
-          
-          incorrectGuesses++;
+            alert('Please enter a valid number between 1 and 6.');
         }
-  
-        // Update the scores
-        scoreElement.textContent = correctGuesses;
-        incorrectElement.textContent = incorrectGuesses;
-
-  
-        // Reset the answer box for the next guess
-        answerBox.value = '';
-  
-        // Generate a new random number for the next round
-        correctNumber = Math.floor(Math.random() * 6) + 1;
-      } else {
-        alert('Please enter a valid number between 1 and 6.');
-      }
     });
-  });
+});
